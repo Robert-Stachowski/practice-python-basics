ad3d55cff7862c9426c0f1a15ea834c525c8391c
Robert Stachowski <RobertStachowski0@gmail.com>
2025-06-24 13:36:34 +0200

    implementacja elif , else

--- 01/app.py
+++ 01/app.py
@@ -1,5 +1,10 @@
-number = 10
+number = 9
 
 if number > 15:
     print("Liczba jest większa niż 15")
-    
+elif number == 10:
+    print("Liczba jest równa 10")
+elif number < 10:
+    print("Liczba jest mniejsza od 10")
+else:
+    print("Liczba jest mniejsza niż 15")
