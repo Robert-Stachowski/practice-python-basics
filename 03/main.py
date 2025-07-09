44afe6a92cf5a8a1247dfe19657121231f1ab2e3
Robert Stachowski <RobertStachowski0@gmail.com>
2025-07-09 12:27:33 +0200

    Final version, stable

--- 03/main.py
+++ 03/main.py
@@ -1,3 +1,7 @@
 def find_max_number(a, b, c):
-    # Uzupełnij kod
-    return 
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:   
+        return c
