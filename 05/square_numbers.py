c3531fd7c57371d8f67e65838f1ea6a09a32420f
Robert Stachowski <RobertStachowski0@gmail.com>
2025-07-09 14:21:11 +0200

    Final version,stable

--- 05/square_numbers.py
+++ 05/square_numbers.py
@@ -1,5 +1,9 @@
 def square_numbers(numbers):
-    result = map(lambda x:x**x, numbers)
+    result = map(lambda x:x**2, numbers)
+    result = list(result)
     return result
 
-print(square_numbers([1, 2, 3, 4])) 
+print(square_numbers([1, -2, 3, 4]))
+#lub tak:
+#result = square_numbers([1, 2, 3, 4])
+#print(result)
