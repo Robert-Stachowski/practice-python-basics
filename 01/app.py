eed309cd7aaed995086e59686b93673a912e964e
Robert Stachowski <RobertStachowski0@gmail.com>
2025-07-09 11:36:24 +0200

    Final version, stable

--- 01/app.py
+++ 01/app.py
@@ -1,7 +1,7 @@
-number = 9
+number = 10
 
-if number > 15:
-    print("Liczba jest większa niż 15")
+if number > 10:
+    print("Liczba jest większa niż 10")
 elif number == 10:
     print("Liczba jest równa 10")
 else: 
