8e54fcb81d693b7ec2b418653bbf94d1e8215506
Robert Stachowski <RobertStachowski0@gmail.com>
2025-07-09 13:43:27 +0200

    Final version, stable

--- 04/analyze_list.py
+++ 04/analyze_list.py
@@ -1,3 +1,12 @@
 def analyze_list(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    count = len(numbers)
+    # byłem przekonany, 
+    # że podstawiamy pod zmienną "numbers" liczby i operujemy na tej zmiennej, 
+    # ale widzę ze jest tu trochę inaczej i definiujemy zmienne na obiekcie/nazwie funkcji na końcu przy wywołaniu,
+    # natomiast sama zmienna zadeklarowana przy funkcji to jest tylko pojemnik na dane...
     # Uzupełnij kod, aby zwrócić długość listy, najmniejszą i największą liczbę
-    return ____________, ____________, ____________
+    return minimum, maximum, count
+minimum, maximum, count = analyze_list([-4, 0, 4, 9, 14])
+print(count, minimum, maximum)
